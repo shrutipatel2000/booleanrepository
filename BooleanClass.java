a55d3de8754ad98d5a4dea00d673199df2cf3744
shruti patel <Shrutipatel203@gmail.com>
2023-10-31 19:44:15 +0000

    booleanrepository

---
+++ BooleanClass.java
@@ -0,0 +1,18 @@
+package booleanpackage;
+
+public class BooleanClass {
+
+    public static void main(String[] args){
+
+        boolean a=true;
+        boolean b=false;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+
+
+    }
+}
